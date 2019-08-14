a3e8d7c3c68a7b754c741c5441c3cbb692661d6a
KingRainbow44 <27646710+KingRainbow44@users.noreply.github.com>
2019-08-14 16:43:00 -0700

    Create EnderPearlCooldown.php

---
+++ EnderPearlCooldown.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+/**
+ * @name EnderPearlCooldownAddon
+ * @version 1.0.0
+ * @main JackMD\ScoreHud\Addons\EnderPearlCooldownAddon
+ * @depend EnderPearlCooldown
+ */
+namespace JackMD\ScoreHud\Addons
+{
+	use JackMD\ScoreHud\addon\AddonBase;
+	use AdminConfirmed\EnderPearlCooldown\Main;
+	use pocketmine\Player;
+	class EnderPearlCooldownAddon extends AddonBase{
+		/** @var EnderPearlCooldown */
+		private $EnderPearlCooldown;
+		public function onEnable(): void{
+			$this->EnderPearlCooldown = $this->getServer()->getPluginManager()->getPlugin("EnderPearlCooldown");
+		}
+		/**
+		 * @param Player $player
+		 * @return array
+		 */
+		public function getProcessedTags(Player $player): array{
+			return [
+				"{combat_duration}" => $this->EnderPearlCooldown->onEnderPearl(PlayerInteractEvent $event)
+			];
+		}
+	}
+}
